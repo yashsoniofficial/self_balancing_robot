d7967b45c329b340da7c4abb9195c25ab8195063
admin <yash.soni@searce.com>
2025-10-02 17:19:45 -0400

    Update - changed motor controller code

--- microcontroller/src/motor_controller.ino
+++ microcontroller/src/motor_controller.ino
@@ -1,76 +1,81 @@
 // motor_controller.ino
-// ESP32 / Arduino sketch
+// ESP32 / Arduino sketch for Self-Balancing Robot
+// Hardware: MPU6050 IMU + L298N Motor Driver
 // Protocol (serial):
 // - MCU -> PC: CSV line: t,ax,ay,az,gx,gy,gz,encL,encR
 // - PC -> MCU: "M,<left_pwm>,<right_pwm>\n" (left/right -127..127 signed)
 
 #include <Wire.h>
 #include "MPU6050.h"
-#include "ESP32Encoder.h"
+#include "Encoder.h"   // If you have encoders, otherwise comment out
 
-// ----- CONFIG -----
-const int PWM_LEFT_PIN = 18;   // PWM PWM channel pins
-const int DIR_LEFT_PIN  = 19;
-const int PWM_RIGHT_PIN = 21;
-const int DIR_RIGHT_PIN = 22;
+// ----- PIN CONFIG (update based on your wiring) -----
+const int ENA = 5;   // PWM pin for Left Motor (L298N ENA)
+const int IN1 = 6;   // Left motor input 1
+const int IN2 = 7;   // Left motor input 2
 
-const int ENC_LEFT_A = 34;
-const int ENC_LEFT_B = 35;
-const int ENC_RIGHT_A = 32;
-const int ENC_RIGHT_B = 33;
+const int ENB = 9;   // PWM pin for Right Motor (L298N ENB)
+const int IN3 = 10;  // Right motor input 1
+const int IN4 = 11;  // Right motor input 2
+
+const int ENC_LEFT_A  = 2;  // Encoder pins (optional)
+const int ENC_LEFT_B  = 3;
+const int ENC_RIGHT_A = 18;
+const int ENC_RIGHT_B = 19;
 
 MPU6050 imu;
-ESP32Encoder encL;
-ESP32Encoder encR;
+Encoder encL(ENC_LEFT_A, ENC_LEFT_B);
+Encoder encR(ENC_RIGHT_A, ENC_RIGHT_B);
 
-// PWM channels for ESP32 LedC
-const int LEFT_CH = 0;
-const int RIGHT_CH = 1;
-const int PWM_FREQ = 20000;
-const int PWM_RES = 8; // 8-bit
-
-long lastPrint = 0;
 char inBuf[64];
 
 void setup() {
   Serial.begin(115200);
   delay(100);
 
-  // IMU init
   Wire.begin();
   imu.initialize();
   if (!imu.testConnection()) {
     Serial.println("IMU FAIL");
   }
 
-  // encoders
-  encL.attachFullQuad(ENC_LEFT_A, ENC_LEFT_B);
-  encR.attachFullQuad(ENC_RIGHT_A, ENC_RIGHT_B);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
 
-  // pwm
-  ledcSetup(LEFT_CH, PWM_FREQ, PWM_RES);
-  ledcSetup(RIGHT_CH, PWM_FREQ, PWM_RES);
-  ledcAttachPin(PWM_LEFT_PIN, LEFT_CH);
-  ledcAttachPin(PWM_RIGHT_PIN, RIGHT_CH);
-  pinMode(DIR_LEFT_PIN, OUTPUT);
-  pinMode(DIR_RIGHT_PIN, OUTPUT);
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
 }
 
-void applyMotor(int pwm, int ch, int dirPin) {
-  if (pwm >= 0) { digitalWrite(dirPin, HIGH); ledcWrite(ch, pwm); }
-  else { digitalWrite(dirPin, LOW); ledcWrite(ch, -pwm); }
+void setMotor(int pwm, int in1, int in2, int enPin) {
+  if (pwm > 0) {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    analogWrite(enPin, pwm);
+  } else if (pwm < 0) {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    analogWrite(enPin, -pwm);
+  } else {
+    // stop
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    analogWrite(enPin, 0);
+  }
 }
 
 void loop() {
-  // read IMU
+  // --- Read IMU ---
   int16_t ax, ay, az, gx, gy, gz;
   imu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-  long encLCount = encL.getCount();
-  long encRCount = encR.getCount();
+  long encLCount = encL.read();
+  long encRCount = encR.read();
 
   unsigned long t = millis();
-  // print CSV
   Serial.print(t); Serial.print(',');
   Serial.print(ax); Serial.print(',');
   Serial.print(ay); Serial.print(',');
@@ -81,28 +86,28 @@ void loop() {
   Serial.print(encLCount); Serial.print(',');
   Serial.println(encRCount);
 
-  // check incoming commands
+  // --- Check incoming motor command ---
   if (Serial.available()) {
-    size_t len = Serial.readBytesUntil('\n', inBuf, sizeof(inBuf)-1);
+    size_t len = Serial.readBytesUntil('\n', inBuf, sizeof(inBuf) - 1);
     inBuf[len] = 0;
-    if (len > 0) {
-      if (inBuf[0] == 'M' && inBuf[1] == ',') {
-        // parse "M,<l>,<r>"
-        int l=0,r=0;
-        char *p = inBuf+2;
-        l = atoi(p);
-        char *comma = strchr(p, ',');
-        if (comma) r = atoi(comma+1);
-        // constrain -127..127 -> map to 8-bit PWM 0..255
-        l = max(-127, min(127, l));
-        r = max(-127, min(127, r));
-        int pwmL = map(abs(l), 0, 127, 0, 255);
-        int pwmR = map(abs(r), 0, 127, 0, 255);
-        applyMotor((l>=0)?pwmL:-pwmL, LEFT_CH, DIR_LEFT_PIN);
-        applyMotor((r>=0)?pwmR:-pwmR, RIGHT_CH, DIR_RIGHT_PIN);
-      }
+    if (len > 0 && inBuf[0] == 'M' && inBuf[1] == ',') {
+      int l = 0, r = 0;
+      char *p = inBuf + 2;
+      l = atoi(p);
+      char *comma = strchr(p, ',');
+      if (comma) r = atoi(comma + 1);
+
+      l = constrain(l, -127, 127);
+      r = constrain(r, -127, 127);
+
+      // map -127..127 -> -255..255
+      int pwmL = map(l, -127, 127, -255, 255);
+      int pwmR = map(r, -127, 127, -255, 255);
+
+      setMotor(pwmL, IN1, IN2, ENA);
+      setMotor(pwmR, IN3, IN4, ENB);
     }
   }
 
-  delay(5); // ~200 Hz telemetry
+  delay(5); // ~200Hz telemetry
 }
